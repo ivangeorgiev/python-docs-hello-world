b8017db07edeca9b3ff5a00408ee0bf4427c9303
Ivan Georgiev <ivan.georgiev@gmail.com>
2020-01-16 23:22:34 +0100

    added print

--- application.py
+++ application.py
@@ -4,3 +4,9 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World!"
+
+
+@app.route("/print")
+def print_route():
+  print("Handling request to home page.")
+  return "Hello Azure!"
