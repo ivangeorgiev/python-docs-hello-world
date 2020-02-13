a9b68863550b393c99dca8a1352bf05354978c49
Ivan Georgiev <ivan.georgiev@gmail.com>
2020-02-13 12:16:55 +0100

    Adding /me and /header routes

--- application.py
+++ application.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask import request, jsonify
+import requests
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -11,8 +13,32 @@ def show_headers():
   return jsonify(dict(request.headers))
 
 
+@app.route("/me")
+def show_me():
+
+  
+  principal_id = request.headers['X-Ms-Client-Principal-Id']
+  access_token = request.headers['X-Ms-Token-Aad-Access-Token']
+
+  headers = {
+    'Authorization': 'Bearer {}'.format(access_token),
+    'User-Agent': 'adal-python-sample',
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  }
+  response = requests.get(
+      "https://graph.microsoft.com/v1.0/users/{}".format(principal_id),
+      headers = headers
+    )
+  json_response = response.json()
+  return jsonify(dict(json_response))
+
 
 @app.route("/print")
 def print_route():
   print("Handling request to home page.")
   return "Hello Azure!"
+
+
+if __name__ == "__main__":
+  app.run(debug=True)
